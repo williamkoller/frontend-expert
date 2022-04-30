0473ba768d31aa823e83a513dbbe9e7c7395ffec
William Koller <williamkoller404@gmail.com>
2022-04-30 17:54:52 -0300

    feat: add icons

---
+++ front-on-food/icons/index.ts
@@ -0,0 +1,19 @@
+import IconBookmark from '../public/images/icons/icons-bookmark.svg';
+import IconCategory from '../public/images/icons/icons-category.svg';
+import IconDelivery from '../public/images/icons/icons-delivery.svg';
+import IconFilter from '../public/images/icons/icons-filter.svg';
+import IconHeart from '../public/images/icons/icons-heart.svg';
+import IconHome from '../public/images/icons/icons-home.svg';
+import IconRestaurant from '../public/images/icons/icons-restaurant.svg';
+import IconReviews from '../public/images/icons/icons-reviews.svg';
+
+export {
+  IconBookmark,
+  IconCategory,
+  IconDelivery,
+  IconFilter,
+  IconHeart,
+  IconHome,
+  IconRestaurant,
+  IconReviews,
+};
