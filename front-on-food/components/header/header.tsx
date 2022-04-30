18607c8783e32876f9252fd9986cd6f7d0fb1295
William Koller <williamkoller404@gmail.com>
2022-04-30 01:04:52 -0300

    feat: add header module css

--- front-on-food/components/header/header.tsx
+++ front-on-food/components/header/header.tsx
@@ -1,7 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Styles from './header.module.css';
+
 export function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      <nav className={Styles.nav}>
+        <div className={Styles.brand}></div>
+        <ul>
+          <li>
+            <Link href='/'>
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/favorites'>
+              <a>Favorites</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
